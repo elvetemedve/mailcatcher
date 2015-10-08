aa0efc151d9c87567af1752c335da10b0d077ad4
Marijus Kilmanas <mkilmanas@inviqa.com>
2015-10-08 12:07:34 +0300

    Fix string interpolation for config param

--- recipes/iptables-ng.rb
+++ recipes/iptables-ng.rb
@@ -1,4 +1,4 @@
 # Setup iptables rule to allow access to mailcatcher http port
 iptables_ng_rule 'mailcatcher' do
-  rule '--protocol tcp --dport {node['mailcatcher']['http_port']} --match state --state NEW,ESTABLISHED --jump ACCEPT'
+  rule "--protocol tcp --dport #{node['mailcatcher']['http_port']} --match state --state NEW,ESTABLISHED --jump ACCEPT"
 end
