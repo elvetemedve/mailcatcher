c09c5b3edfbd576399b3a7827d7d3bceb4794da7
Marijus Kilmanas <mkilmanas@inviqa.com>
2015-10-08 11:19:30 +0300

    Changes to the recipe and config to get it working

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,12 @@
+default['mailcatcher']['ruby_version'] = '1.9.3-p545'
+
 default['mailcatcher']['http_ip'] = '0.0.0.0'
 default['mailcatcher']['http_port'] = 1080
 
 default['mailcatcher']['smtp_ip'] = '127.0.0.1'
 default['mailcatcher']['smtp_port'] = 1025
 
-default['mailcatcher']['bin'] = 'mailcatcher'
+default['mailcatcher']['bin'] = '/opt/rbenv/shims/mailcatcher'
 default['mailcatcher']['version'] = '0.6.1'
 
-default['mailcatcher']['catchmail_bin'] = 'catchmail'
+default['mailcatcher']['catchmail_bin'] = '/opt/rbenv/shims/catchmail'
