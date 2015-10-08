53a9dd75c453c3ab79825ae79a7ca83d292f96c8
Marijus Kilmanas <mkilmanas@inviqa.com>
2015-10-08 11:59:58 +0300

    Add iptables recipe to setup http port rule

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ description       "Allows for installation of MailCatcher on CentOS boxes."
 version           "0.0.2"
 
 depends "rbenv"
+depends "iptables-ng"
 
 recommends "php"
 recommends "php-fpm"
