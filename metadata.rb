ef15ea3e3c8fee9cfdb646985af2c2bacae5664d
Geza Buza <gbuza@sessiondigital.com>
2015-10-27 09:54:45 +0100

    Increment version number to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "mailcatcher-ng"
 maintainer        "Inviqa"
 license           "Apache 2.0"
 description       "Allows for installation of MailCatcher on CentOS boxes."
-version           "0.1.0"
+version           "0.1.1"
 
 depends "rbenv"
 depends "postfix"
